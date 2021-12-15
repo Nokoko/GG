43206686fc680e83372ad2140d04adf7a9c7b100
Nokoko <iris-z@hotmail.fr>
2021-12-15 11:45:03 +0100

    Update GGTeemo.lua

--- GGTeemo.lua
+++ GGTeemo.lua
@@ -12,4 +12,210 @@ local Script = {
 
 module(Script.Name, package.seeall, log.setup)
 clean.module(Script.Name, clean.seeall, log.setup)
-CoreEx.AutoUpdate("https://robur.site/shulepin/robur/raw/branch/master/" .. Script.Name .. ".lua", Script.Version)
+CoreEx.AutoUpdate("https://github.com/Nokoko/GG/blob/main/" .. Script.Name .. ".lua", Script.Version)
+
+local SDK = _G.CoreEx
+local Player = _G.Player
+local DreamEvade = _G.DreamEvade
+
+local DamageLib, CollisionLib, DashLib, HealthPred, ImmobileLib, Menu, Orbwalker, Prediction, Profiler, Spell, TargetSelector =
+_G.Libs.DamageLib, _G.Libs.CollisionLib, _G.Libs.DashLib, _G.Libs.HealthPred, _G.Libs.ImmobileLib, _G.Libs.NewMenu,
+_G.Libs.Orbwalker, _G.Libs.Prediction, _G.Libs.Profiler, _G.Libs.Spell, _G.Libs.TargetSelector()
+
+local AutoUpdate, Enums, EvadeAPI, EventManager, Game, Geometry, Input, Nav, ObjectManager, Renderer =
+SDK.AutoUpdate, SDK.Enums, SDK.EvadeAPI, SDK.EventManager, SDK.Game, SDK.Geometry, SDK.Input, SDK.Nav, SDK.ObjectManager, SDK.Renderer
+
+local AbilityResourceTypes, BuffTypes, DamageTypes, Events, GameMaps, GameObjectOrders, HitChance, ItemSlots, 
+ObjectTypeFlags, PerkIDs, QueueTypes, SpellSlots, SpellStates, Teams = 
+Enums.AbilityResourceTypes, Enums.BuffTypes, Enums.DamageTypes, Enums.Events, Enums.GameMaps, Enums.GameObjectOrders,
+Enums.HitChance, Enums.ItemSlots, Enums.ObjectTypeFlags, Enums.PerkIDs, Enums.QueueTypes, Enums.SpellSlots, Enums.SpellStates,
+Enums.Teams
+
+local Vector, BestCoveringCircle, BestCoveringCone, BestCoveringRectangle, Circle, CircleCircleIntersection,
+Cone, LineCircleIntersection, Path, Polygon, Rectangle, Ring =
+Geometry.Vector, Geometry.BestCoveringCircle, Geometry.BestCoveringCone, Geometry.BestCoveringRectangle, Geometry.Circle,
+Geometry.CircleCircleIntersection, Geometry.Cone, Geometry.LineCircleIntersection, Geometry.Path, Geometry.Polygon,
+Geometry.Rectangle, Geometry.Ring
+
+local abs, acos, asin, atan, ceil, cos, deg, exp, floor, fmod, huge, log, max, min, modf, pi, rad, random, randomseed, sin,
+sqrt, tan, type, ult = 
+_G.math.abs, _G.math.acos, _G.math.asin, _G.math.atan, _G.math.ceil, _G.math.cos, _G.math.deg, _G.math.exp,
+_G.math.floor, _G.math.fmod, _G.math.huge, _G.math.log, _G.math.max, _G.math.min, _G.math.modf, _G.math.pi, _G.math.rad,
+_G.math.random, _G.math.randomseed, _G.math.sin, _G.math.sqrt, _G.math.tan, _G.math.type, _G.math.ult
+
+local byte, char, dump, ends_with, find, format, gmatch, gsub, len, lower, match, pack, packsize, rep, reverse,
+starts_with, sub, unpack, upper = 
+_G.string.byte, _G.string.char, _G.string.dump, _G.string.ends_with, _G.string.find, _G.string.format,
+_G.string.gmatch, _G.string.gsub, _G.string.len, _G.string.lower, _G.string.match, _G.string.pack, _G.string.packsize,
+_G.string.rep, _G.string.reverse, _G.string.starts_with, _G.string.sub, _G.string.unpack, _G.string.upper
+
+local clock, date, difftime, execute, exit, getenv, remove, rename, setlocale, time, tmpname = 
+_G.os.clock, _G.os.date, _G.os.difftime, _G.os.execute, _G.os.exit, _G.os.getenv, _G.os.remove, _G.os.rename, _G.os.setlocale,
+_G.os.time, _G.os.tmpname
+
+local Resolution = Renderer.GetResolution()
+
+local LMBPressed = false
+local HitChanceList = { "Collision", "OutOfRange", "VeryLow", "Low", "Medium", "High", "VeryHigh", "Dashing", "Immobile" }
+
+local AddWhiteListMenu = function(id, name)
+    local name = name or "White List"
+    Menu.NewTree(id, name, function()
+        for k, hero in pairs(ObjectManager.Get("enemy", "heroes")) do
+            local heroAI = hero.AsAI
+            Menu.Checkbox(id .. heroAI.CharName, heroAI.CharName, true)
+        end
+    end)
+end
+
+local CursorIsUnder = function(x, y, sizeX, sizeY)
+    local mousePos = Renderer.GetCursorPos()
+    if not mousePos then
+        return false
+    end
+    local posX, posY = mousePos.x, mousePos.y
+    if sizeY == nil then
+        sizeY = sizeX
+    end
+    if sizeX < 0 then
+        x = x + sizeX
+        sizeX = -sizeX
+    end
+    if sizeY < 0 then
+        y = y + sizeY
+        sizeY = -sizeY
+    end
+    return posX >= x and posX <= x + sizeX and posY >= y and posY <= y + sizeY
+end
+
+local InfoPanel = {
+    X = 100,
+    Y = 100,
+    Size = Vector(200, 22),
+    Color = 0x000000AA,
+    Font = Renderer.CreateFont("Bahnschrift.ttf", 20),
+    Options = {
+        [1] = {
+            Text = "Spell Farm",
+            Type = 0,
+        }
+    },
+    SpellFarmStatus = Menu.Get("GGScript.InfoPanel.SpellFarmStatus", true),
+    SpellFarmStatusT = 0,
+    MoveOffset = {},
+    MenuCreated = false,
+	
+	}
+
+function InfoPanel.CreateMenu()
+    Menu.NewTree("GGScript.InfoPanel", "Information Panel", function()
+        Menu.Checkbox("GGScript.InfoPanel.SpellFarmStatus", "Spell Farm Status", true)
+        Menu.Text("X - "); Menu.SameLine(); Menu.Slider("GGScript.InfoPanel.X", "", 100, 0, Resolution.x, 1)
+        Menu.Text("Y - "); Menu.SameLine(); Menu.Slider("GGScript.InfoPanel.Y", "", 100, 0, Resolution.y, 1)
+    end)
+    InfoPanel.MenuCreated = true
+end
+
+function InfoPanel.AddOption(option)
+    InfoPanel.Options[#InfoPanel.Options + 1] = option
+end
+
+EventManager.RegisterCallback(Events.OnDraw, function()
+    if not InfoPanel.MenuCreated then return end
+
+    InfoPanel.X = Menu.Get("GGScript.InfoPanel.X")
+    InfoPanel.Y = Menu.Get("GGScript.InfoPanel.Y")
+    local font = InfoPanel.Font
+
+    InfoPanel.Size.y = 22 + (#InfoPanel.Options * 20)
+
+    Renderer.DrawFilledRect(Vector(InfoPanel.X, InfoPanel.Y), InfoPanel.Size, 0, InfoPanel.Color)
+
+    local text = "GGTeemo"
+    local textExtent = font:CalcTextSize(tostring(text))
+    local textPosition = Vector(InfoPanel.X + ((InfoPanel.Size.x / 2) - (textExtent.x / 2)), InfoPanel.Y)
+    font:DrawText(textPosition, text, 0xFFFFFFFF)
+
+    for k, v in ipairs(InfoPanel.Options) do
+        local text = v.Text
+        local textPosition = Vector(InfoPanel.X + 5, InfoPanel.Y + (k * 20))
+        
+        if v.Type == 0 then
+            local menuValue = Menu.Get("GGScript.InfoPanel.SpellFarmStatus", true)
+            local status = menuValue and "Enabled" or "Disabled"
+            local color = menuValue and 0x28cf4cFF or 0xdf2626FF
+            local textExtent = font:CalcTextSize(tostring(text))
+            font:DrawText(textPosition, text, color)
+            font:DrawText(Vector(textPosition.x + textExtent.x , textPosition.y), " [Scroll Down]", 0xa9a7a7FF  )
+        elseif v.Type == 1 then
+            font:DrawText(textPosition, text, 0xFFFFFFFF)
+        end
+    end
+
+    do
+        local cursorPos = Renderer.GetCursorPos()
+        local rect = {x = InfoPanel.X - 5, y = InfoPanel.Y - 5, z = InfoPanel.Size.x, w = InfoPanel.Size.y}
+        if not InfoPanel.MoveOffset and rect and CursorIsUnder(rect.x, rect.y, rect.z, rect.w) and LMBPressed then
+            InfoPanel.MoveOffset = {
+                x = rect.x - cursorPos.x + 5,
+                y = rect.y - cursorPos.y + 5
+            }
+        elseif InfoPanel.MoveOffset and not LMBPressed then
+            InfoPanel.MoveOffset = nil
+        end
+
+        if InfoPanel.MoveOffset and rect and rect.x and rect.y then
+            rect.x = InfoPanel.MoveOffset.x + cursorPos.x
+            rect.x = rect.x > 0 and rect.x or 0
+            rect.x = rect.x < Resolution.x - rect.z and rect.x or Resolution.x - rect.z
+    
+            rect.y = InfoPanel.MoveOffset.y + cursorPos.y
+            rect.y = rect.y > 0 and rect.y or 0
+            rect.y = rect.y < (Resolution.y - rect.w + 6) and rect.y or (Resolution.y - rect.w + 6)
+    
+            if LMBPressed then
+                InfoPanel.X = rect.x
+                InfoPanel.Y = rect.y
+                Menu.Set("GGScript.InfoPanel.X", rect.x)
+                Menu.Set("GGScript.InfoPanel.Y", rect.y)
+            end
+        end
+    end
+end)
+
+EventManager.RegisterCallback(Events.OnMouseEvent, function(e, message, wparam, lparam)
+    LMBPressed = e == 513
+    if e == 522 and InfoPanel.SpellFarmStatusT + 0.25 < Game.GetTime() then
+        InfoPanel.SpellFarmStatus = not InfoPanel.SpellFarmStatus
+        Menu.Set("GGScript.InfoPanel.SpellFarmStatus", InfoPanel.SpellFarmStatus)
+        InfoPanel.SpellFarmStatusT = Game.GetTime()
+    end
+end)*
+
+function Teemo.Initialize()
+    Teemo.CreateMenu()
+    Teemo.CreateSpells()
+    Teemo.CreateEvents()
+end
+
+function Teemo.CreateSpells()
+    Teemo.Spells = {}
+
+    Teemo.Spells["Q"] = Spell.Skillshot({
+        Slot            = SpellSlots.Q,
+        Range           = 1250,
+        Width           = 180,
+        Delay           = 0.25,
+        Speed           = 1350,
+        Type            = "Linear",
+        Collisions      = { WindWall = true },
+    })
+
+    Teemo.Spells["W"] = Spell.Active({
+        Slot            = SpellSlots.W
+    })
+
+    Teemo.Spells["E"] = Spell.Active({
+        Slot            = SpellSlots.E
+    })
+end
